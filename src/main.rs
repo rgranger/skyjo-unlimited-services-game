a127fb006ff8d2892e4fb21734d4661e692d2d73
Robin Granger <robin.granger@analogway.com>
2022-07-25 19:58:14 +0200

    feat(skyjo): ajoute mise en place du jeu Skyjo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+mod skyjo;
+
+use skyjo::Game;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let game = Game::new(3);
+
+    println!("{:?}", game);
 }
---
+++ src/skyjo/mod.rs
@@ -0,0 +1,84 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+#[derive(Debug)]
+pub struct Game {
+    first_player: Option<u8>,
+    players: Vec<Player>,
+    draw_pile: Vec<Card>,
+    discard_pile: Vec<Card>,
+}
+
+impl Game {
+    pub fn new(player_count: u8) -> Game {
+        assert!((2..=8).contains(&player_count));
+
+        Game {
+            first_player: None,
+            players: (0..player_count).map(|_| Player::new()).collect(),
+            draw_pile: create_deck(),
+            discard_pile: Vec::new(),
+        }
+    }
+}
+
+fn create_deck() -> Vec<Card> {
+    let mut cards = Vec::new();
+    (0..5).for_each(|_| cards.push(Card::new(-2)));
+    (0..10).for_each(|_| cards.push(Card::new(-1)));
+    (0..15).for_each(|_| cards.push(Card::new(0)));
+    (0..10).for_each(|_| cards.push(Card::new(1)));
+    (0..10).for_each(|_| cards.push(Card::new(2)));
+    (0..10).for_each(|_| cards.push(Card::new(3)));
+    (0..10).for_each(|_| cards.push(Card::new(4)));
+    (0..10).for_each(|_| cards.push(Card::new(5)));
+    (0..10).for_each(|_| cards.push(Card::new(6)));
+    (0..10).for_each(|_| cards.push(Card::new(7)));
+    (0..10).for_each(|_| cards.push(Card::new(8)));
+    (0..10).for_each(|_| cards.push(Card::new(9)));
+    (0..10).for_each(|_| cards.push(Card::new(10)));
+    (0..10).for_each(|_| cards.push(Card::new(11)));
+    (0..10).for_each(|_| cards.push(Card::new(12)));
+
+    cards
+}
+
+#[derive(Debug)]
+struct Player {
+    score: u16,
+    cards: PlayerCards,
+}
+
+impl Player {
+    fn new() -> Player {
+        Player {
+            score: 0,
+            cards: [None, None, None, None],
+        }
+    }
+}
+
+type PlayerCards = [Option<[Card; 3]>; 4];
+
+#[derive(Debug)]
+struct Card {
+    value: i8,
+    status: CardStatus,
+}
+
+impl Card {
+    fn new(value: i8) -> Card {
+        assert!((-2..=12).contains(&value));
+
+        Card {
+            value,
+            status: CardStatus::Hidden,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum CardStatus {
+    Shown,
+    Hidden,
+}
